94e45249bdb6b9ce573695315d4f3a6cc4540713
Daniel Hug <dhug@albany.edu>
2016-09-18 12:58:15 -0400

    Created getters/setters for the Food class

--- HomeworkOne/src/Food.java
+++ HomeworkOne/src/Food.java
@@ -1,6 +1,42 @@
 //Created by Daniel Hug
 //Class for all food in the RPG
-public class Food {
+public class Food extends Item {
+	int nutrition; //How healthy the food 
+	int quantity; //How much food
+	
+	//Constructor for food class
+	public Food(String name, int weight, int nutrition, int quantity ){
+		super(name, weight);
+		this.nutrition = nutrition;
+		this.quantity = quantity;
+		
+	}
+	
+	//Method to get nutrition value
+	public int getNutrition(){
+		return this.nutrition;
+		
+	}
+	
+	//Method that gets quantity of food
+	public int getQuantity(){
+		return this.quantity;
+		
+	}
+	
+	//Method that sets nutrition value
+	public void setNutrition(int nutrition){
+		this.nutrition = nutrition;
+	}
+	
+	//Method that sets the quantity
+	public void setQuantity(int quantity){
+		this.quantity = quantity;
+	}
+	
+	
+	
+	
 	
 
 }
