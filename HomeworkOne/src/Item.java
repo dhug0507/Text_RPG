dccda9a4d120a59cedbb6d12a8b48bbdade8fd62
Daniel Hug <dhug@albany.edu>
2016-09-17 20:54:40 -0400

    initialized properties, created a constructor, and made getters/setters for Item class

--- HomeworkOne/src/Item.java
+++ HomeworkOne/src/Item.java
@@ -1,4 +1,17 @@
-
+//Created by Daniel Hug
+//Parent class for all Items in RPG
 public class Item {
+	//Initialize properties
+	private String name = ""; //Name of the Item
+	//made a String array, separated weight into 3 different categories light, medium, and heavy.
+	private String[] weight = {"Light", "Medium", "Heavy"}; 
+	
+	//Constructor
+	public Item(String name, String[] weight){
+		this.name = name;
+		this.weight = weight;
+		
+	}
+	
 
 }
