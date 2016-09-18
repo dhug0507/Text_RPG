fbdeee0146df4610dfe726fcd6d6a97ea6752a58
Daniel Hug <dhug@albany.edu>
2016-09-18 19:18:57 -0400

    Added multiple tests for the armor and weapon class

--- HomeworkOne/src/RpgMain.java
+++ HomeworkOne/src/RpgMain.java
@@ -5,10 +5,14 @@ public class RpgMain {
  public static void main(String[] args) {
    int[] defense = {15,25,20,10,12}; //{"Helmet", "Breast_Plate", "Legs", "Gloves", "Boots"};
    int armorWeight = 100;
+   int damage = 10;
+   int speed = 2;
+   int weaponWeight = 30;
+     
    
-   
+   //This object creation call the default armor constructor
       Armor invetory = new Armor("Plate Armor", armorWeight, defense);
-      System.out.println("Armor name: " + invetory.getName());
+      System.out.println("Armor name: " + invetory.getName()); //Test getName method 
       System.out.println("Total Armor Weight: " + invetory.getWeight() + " pounds");
       System.out.println("Total Armor Defense: " + (invetory.getDefense()[0]+
                                                     invetory.getDefense()[1]+
@@ -20,6 +24,13 @@ public class RpgMain {
       System.out.println("Leg Defense: " +  invetory.getDefense()[2]);
       System.out.println("Gloves Defense: " +  invetory.getDefense()[3]);
       System.out.println("Boots Defense: " +  invetory.getDefense()[4]);
+      
+      Weapon armory = new Weapon("Sword of a Thousand Truths", weaponWeight, damage, speed);
+      System.out.println("Weapons name: " + armory.getName());
+      System.out.println("Weapon's weight: " + armory.getWeight() + " Pounds");
+      System.out.println("Attack Power: " + (armory.getDamage() * armory.getSpeed()));
+      
+      
       
   
   
--- HomeworkOne/src/Weapon.java
+++ HomeworkOne/src/Weapon.java
@@ -1,32 +1,42 @@
 //Created by Daniel Hug
 //Class for all weapons in RPG
 public class Weapon extends Item {
-	private int damage;
-	private String[] weaponType = {"Sword", "Axe", "Mace"};
-	
-	
-	public Weapon(String name, 
-			      int weight, 
-			      int damage, 
-			      String[] weaponType){ 
-		super(name, weight);
-		this.damage = damage;
-		this.weaponType = weaponType;
-	}
-	
-	public int getDamage(){
-		return this.damage;
+  private int damage;// = new int[3];//Sword, Axe, Mace
+ private int speed; //= new int[3];//The speed of the Sword, Axe, Mace
+ 
+ 
+ //Constructor for the Wepons class
+ public Weapon(String name, 
+         int weight, 
+         int damage,
+         int speed){ 
+  super(name, weight);
+  this.damage = damage;
+  this.speed = speed;
+  
+ }
+ 
+ //Method that gets the Damage stat
+ public int getDamage(){
+  return this.damage;
 
-	}
-	public String[] getWeaponType(){
-		return this.weaponType;
-	}
-	
-	public void setDamage(int damage){
-		this.damage = damage;
-	}
-	
-	public void setWeaponType(String[] weaponType){
-		this.weaponType = weaponType;
-	}
+ }
+ 
+ //Method that gets the speed stat
+ public int getSpeed(){
+  return this.speed;
+
+ }
+
+ //Method that sets the Damage stat
+ public void setDamage(int damage){
+  this.damage = damage;
+ }
+ 
+ //Method that sets the speed stat
+  public void setSpeed(int speed){
+  this.speed = speed;
+ }
+ 
+
 }
